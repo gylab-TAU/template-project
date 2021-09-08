a30ab667a679c5e4b6d4c7b7cb68df54f34e4a00
Gali Geller <gali1998@gmai.com>
2021-09-08 22:54:34 +0300

    working experiment

--- src/experiment.js
+++ src/experiment.js
@@ -16,11 +16,17 @@ import "../styles/main.scss";
 // jsPsych plugins
 import "jspsych/plugins/jspsych-html-keyboard-response";
 import "jspsych/plugins/jspsych-fullscreen";
+import "jspsych/plugins/jspsych-call-function";
+import "jspsych/jspsych";
 
 
 import * as consent from "./components/consentComponent";
 import * as id from "./components/idComponent";
 import * as instructions from "./components/instructionsComponent";
+
+import { showStimProcedure } from "./procedures/showStimProcedure";
+
+import axios from "axios";
 /**
  * This is where you define your jsPsych timeline.
  *
@@ -48,12 +54,57 @@ export function createTimeline(input = {}) {
 
   timeline.push(instructions.default.getTrial());
 
+  timeline.push((new showStimProcedure("stimuli", "stim", 4, "jpg")).getProcedure());
+
+
+  let sendData = {
+    type: 'call-function',
+    func: () => { 
+      let first_trial = jsPsych.data.get().values()[0];
+      let participantId = first_trial["participantId"];
+      console.log(participantId)
+      console.log(jsPsych.data.get().values())
+      sendDataToNutella("Gali", "jspsych-attempt", jsPsych.data.get().values(), participantId);
+     }
+  }
+
+  timeline.push(sendData);
+
+  let endMessage = {
+    type: 'html-keyboard-response',
+    stimulus: '<p style="font-size: 48px;">Thank you!</p>',
+    choices: jsPsych.NO_KEYS
+  };
+
+  timeline.push(endMessage)
+
   return timeline;
 }
 
-export function on_finish(message) {
-  
+function sendDataToNutella(experimenterName, experimentName, data, participantId) {
+  let postObject = {
+    "data": {
+      "participant_info": {
+        "participant_id": participantId,
+      },
+      "time": Date.now(),
+      "headers": ["fake hraders"],
+      "trials": data,
+      "experiment_info": {
+        "experimenter_name": experimenterName,
+        "experiment_name": experimentName
+      },
+      "others": {}
+    }
+  }
+  axios.post("http://178.62.106.190/saveResults/", postObject).then(() => {
+    return true;
+  }).catch(() => {
+    return false;
+  });
 }
+
+export function on_finish() {}
 
 // Whatever you `export` from this file will be passed to `jsPsych.init()` (except for `timeline`,
 // which is determined using `createTimeline()`)
